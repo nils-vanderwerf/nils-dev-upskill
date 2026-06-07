ebafd82175be3e5ecbf1200c9a541308cb1a59bf
Nils van der Werf <n.vanderw.92@gmail.com>
2026-06-07 14:57:42 +1000

    Build journaling app

---
+++ React/journaling-app/src/App.tsx
@@ -0,0 +1,25 @@
+export const App = () => {
+  return (
+    <>
+      <div className='flex w-[640px] flex-col py-4 mx-auto'>
+        <div className="flex flex-col gap-2 rounded-xl bg-white p-4">
+          <header>
+            <h1 className="mb-4 text-2xl font-bold border-b border-gray-300 pb-3">
+              Journal App
+            </h1>
+            <p>
+              Embrace each day with reflection: Capture your moments,
+              chart your growth, and craft your journey, one story at
+              a time.
+            </p>
+          </header>
+        </div>
+        {/* Tabs */}
+
+        {/* Form */}
+      </div>
+    </>
+  )
+}
+
+export default App
